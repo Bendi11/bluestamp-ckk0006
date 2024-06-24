0aee1518a4c8fd32e401ad9b744894c72e992331
Bendi11 <bkliebmann@gmail.com>
2024-06-24 09:53:58 -0700

    Removed Lesson 7 library, added commented code

--- Lesson 7 - Control The Robot Arm/Code/Arm/Arm.ino
+++ Lesson 7 - Control The Robot Arm/Code/Arm/Arm.ino
@@ -1,183 +1,87 @@
-/*
- * This code applies to cokoino mechanical arm
- * Through this link you can download the source code:
- * https://github.com/Cokoino/CKK0006
- * Company web site:
- * http://cokoino.com/
- *                                     ________
- *                         ----|servo4| 
- *                        |            --------
- *                    |servo3|   
- *                        |
- *                        |
- *                    |servo2|
- *                        |
- *                        |
- *                  ___________
- *                  |  servo1 |
- *         ____________________
- *         ____________________
- * Fanctions:
- * arm.servo1.read();   //read the servo of angle
- * arm.servo2.read();
- * arm.servo3.read();
- * arm.servo4.read();
- * 
- * arm.servo1.write(angle);   //servo run
- * arm.servo2.write(angle);
- * arm.servo3.write(angle);
- * arm.servo4.write(angle);
- * 
- * arm.left(speed);    //perform the action 
- * arm.right(speed);
- * arm.up(speed);
- * arm.down(speed);
- * arm.open(speed);
- * arm.close(speed);
- * 
- * arm.captureAction();    //capture the current action,return pointer array
- * arm.do_action(int *p,int speed);  //P is a pointer to the array
- * 
- * arm.JoyStickL.read_x(); //Returns joystick numerical
- * arm.JoyStickL.read_y();
- * arm.JoyStickR.read_x();
- * arm.JoyStickR.read_y();
- */
-#include "src/CokoinoArm.h"
-#define buzzerPin 9
+#include <Servo.h>
 
-CokoinoArm arm;
-int xL,yL,xR,yR;
+//The digital pin number that the base servo is connected to.
+//We name it 'azimuth' to be more clear, but this is just the lowest servo on the arm.
+static const unsigned PIN_AZIMUTH = 4;
 
-const int act_max=10;    //Default 10 action,4 the Angle of servo
-int act[act_max][4];    //Only can change the number of action
-int num=0,num_do=0;
-///////////////////////////////////////////////////////////////
-void turnUD(void){
-  if(xL!=512){
-    if(0<=xL && xL<=100){arm.up(10);return;}
-    if(900<xL && xL<=1024){arm.down(10);return;} 
-    if(100<xL && xL<=200){arm.up(20);return;}
-    if(800<xL && xL<=900){arm.down(20);return;}
-    if(200<xL && xL<=300){arm.up(25);return;}
-    if(700<xL && xL<=800){arm.down(25);return;}
-    if(300<xL && xL<=400){arm.up(30);return;}
-    if(600<xL && xL<=700){arm.down(30);return;}
-    if(400<xL && xL<=480){arm.up(35);return;}
-    if(540<xL && xL<=600){arm.down(35);return;} 
+//We have two servo motors in the actual arm that control the height of the arm,
+//which we'll call the 'elevation' servos.
+//The base elevation servo is the one just above the base plate.
+static const unsigned PIN_BASE_ELEVATION = 5;
+
+//The wrist elevation servo is located behind the main hand, and controls the angle that the hand is at.
+static const unsigned PIN_WRIST_ELEVATION = 6;
+
+//The claw servo is on the hand of the arm, and can open and close the two claws when angles are written to it.
+static const unsigned PIN_CLAW = 7;
+
+//We name the analog input pins that each joystick axis is connected to here,
+//so that it's more clear than just writing A0 everywhere we mean to say the left joystick x axis
+static const unsigned
+    PIN_LEFT_JOYSTICK_X = A0,
+    PIN_LEFT_JOYSTICK_Y = A1,
+    PIN_RIGHT_JOYSTICK_X = A2,
+    PIN_RIGHT_JOYSTICK_Y = A3;
+
+//We have four servo motors to control, and we use the `Servo` library to control each.
+//This library provides the Servo **class**, which allows us to easily write and read angles from servos that
+//are connected to the Arduino.
+Servo azimuth;
+Servo base_elevation;
+Servo wrist_elevation;
+Servo claw;
+
+/// Process a raw joystick value to get a change in angle between -5 and 5.
+/// Scales the input reading and applies a deadzone to the joystick.
+/// A **deadzone** is a small circle in the center of a joystick where movements will not affect the angle of the arm.
+int scale_joystick(int adc_reading) {
+    // The value `adc_reading` will be from 0 to 1024, so we must scale this by 100 to put it in the range of 0 to 10.
+    // After this, we subtract 5 to shift the entire range from [0, 10] to [-5, 5]
+    adc_reading = adc_reading / 100 - 5;
+    
+    // Apply a deadzone to the reading - if the value is in the range [-1, 1] then we set it to 0 to ignore the reading.
+    if(adc_reading >= -1 && adc_reading <= 1) {
+        adc_reading = 0;
     }
+    
+    // Return the scaled and processed joystick value to the user
+    return adc_reading;
 }
-///////////////////////////////////////////////////////////////
-void turnLR(void){
-  if(yL!=512){
-    if(0<=yL && yL<=100){arm.right(0);return;}
-    if(900<yL && yL<=1024){arm.left(0);return;}  
-    if(100<yL && yL<=200){arm.right(5);return;}
-    if(800<yL && yL<=900){arm.left(5);return;}
-    if(200<yL && yL<=300){arm.right(10);return;}
-    if(700<yL && yL<=800){arm.left(10);return;}
-    if(300<yL && yL<=400){arm.right(15);return;}
-    if(600<yL && yL<=700){arm.left(15);return;}
-    if(400<yL && yL<=480){arm.right(20);return;}
-    if(540<yL && yL<=600){arm.left(20);return;}
-  }
-}
-///////////////////////////////////////////////////////////////
-void turnCO(void){
-  if(xR!=512){
-    if(0<=xR && xR<=100){arm.close(0);return;}
-    if(900<xR && xR<=1024){arm.open(0);return;} 
-    if(100<xR && xR<=200){arm.close(5);return;}
-    if(800<xR && xR<=900){arm.open(5);return;}
-    if(200<xR && xR<=300){arm.close(10);return;}
-    if(700<xR && xR<=800){arm.open(10);return;}
-    if(300<xR && xR<=400){arm.close(15);return;}
-    if(600<xR && xR<=700){arm.open(15);return;}
-    if(400<xR && xR<=480){arm.close(20);return;}
-    if(540<xR && xR<=600){arm.open(20);return;} 
-    }
-}
-///////////////////////////////////////////////////////////////
-void date_processing(int *x,int *y){
-  if(abs(512-*x)>abs(512-*y))
-    {*y = 512;}
-  else
-    {*x = 512;}
-}
-///////////////////////////////////////////////////////////////
-void buzzer(int H,int L){
-  while(yR<420){
-    digitalWrite(buzzerPin,HIGH);
-    delayMicroseconds(H);
-    digitalWrite(buzzerPin,LOW);
-    delayMicroseconds(L);
-    yR = arm.JoyStickR.read_y();
-    }
-  while(yR>600){
-    digitalWrite(buzzerPin,HIGH);
-    delayMicroseconds(H);
-    digitalWrite(buzzerPin,LOW);
-    delayMicroseconds(L);
-    yR = arm.JoyStickR.read_y();
-    }
-}
-///////////////////////////////////////////////////////////////
-void C_action(void){
-  if(yR>800){
-    int *p;
-    p=arm.captureAction();
-    for(char i=0;i<4;i++){
-    act[num][i]=*p;
-    p=p+1;     
-    }
-    num++;
-    num_do=num;
-    if(num>=act_max){
-      num=0;
-      buzzer(600,400);
-      }
-    while(yR>600){yR = arm.JoyStickR.read_y();}
-    //Serial.println(act[0][0]);
-  }
-}
-///////////////////////////////////////////////////////////////
-void Do_action(void){
-  if(yR<220){
-    buzzer(200,300);
-    for(int i=0;i<num_do;i++){
-      arm.do_action(act[i],15);
-      }
-    num=0;
-    while(yR<420){yR = arm.JoyStickR.read_y();}
-    for(int i=0;i<2000;i++){
-      digitalWrite(buzzerPin,HIGH);
-      delayMicroseconds(200);
-      digitalWrite(buzzerPin,LOW);
-      delayMicroseconds(300);        
-    }
-  }
-}
-///////////////////////////////////////////////////////////////
+
 void setup() {
-  //Serial.begin(9600);
-  //arm of servo motor connection pins
-  arm.ServoAttach(4,5,6,7);
-  //arm of joy stick connection pins : xL,yL,xR,yR
-  arm.JoyStickAttach(A0,A1,A2,A3);
-  pinMode(buzzerPin,OUTPUT);
-}
-///////////////////////////////////////////////////////////////
-void loop() {
-  xL = arm.JoyStickL.read_x();
-  yL = arm.JoyStickL.read_y();
-  xR = arm.JoyStickR.read_x();
-  yR = arm.JoyStickR.read_y();
-  date_processing(&xL,&yL);
-  date_processing(&xR,&yR);
-  turnUD();
-  turnLR();
-  turnCO();
-  C_action();
-  Do_action();
+    Serial.begin(9600);
+    
+    //Let the Arduino know what pins each servo is connected to by calling the attach() method.
+    azimuth.attach(PIN_AZIMUTH);
+    base_elevation.attach(PIN_BASE_ELEVATION);
+    wrist_elevation.attach(PIN_WRIST_ELEVATION);
+    claw.attach(PIN_CLAW);
 }
 
+void loop() {
+    //First, let's read all four joystick values from the pins that they're connected to.
+    //This reads the raw voltage delivered to each pin from 0 to 1024, where 0 is zero volts and 1024 is the
+    //supply voltage of the arduino (usually 5V).
+    // We must process this value to get a small shift to apply to the angle of each servo
+    int left_x  = analogRead(PIN_LEFT_JOYSTICK_X);
+    int left_y  = analogRead(PIN_LEFT_JOYSTICK_Y);
+    int right_x = analogRead(PIN_RIGHT_JOYSTICK_X);
+    int right_y = analogRead(PIN_RIGHT_JOYSTICK_Y);
+    
+    //Now take the raw analog readings, scale them and apply a deadzone, and store the result to four variables
+    //that will be used to change the angle of each servo.
+    int angle_azimuth = scale_joystick(left_x);
+    int angle_elevation_base = scale_joystick(left_y);
+    int angle_elevation_wrist = scale_joystick(right_y);
+    int angle_claw = scale_joystick(right_x);
+    
+    //Change the angle of all four servos by adding the small angle change we got from `scale_joystick` to the current angle of the servo.
+    //The method servo.read() will return an angle between 0 and 180, and servo.write(angle) will change the servo's angle to what is in the smooth braces.
+    azimuth.write(azimuth.read() + angle_azimuth);
+    base_elevation.write(base_elevation.read() + angle_elevation_base);
+    wrist_elevation.write(wrist_elevation.read() + angle_elevation_wrist);
+    claw.write(claw.read() + angle_claw);
+    
+    //Now we wait 30 milliseconds so that the loop repeats about 30 times per second
+    delay(30);
+}
